737f10e888dec2b1c939b281a291c0df5063252d
Scott Davis <stdavis@utah.gov>
2020-02-17 14:58:02 -0700

    feat: finish basic implementation of layer-selector

--- projects/layer-selector/src/lib/layer-selector.module.ts
+++ projects/layer-selector/src/lib/layer-selector.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { LayerSelectorComponent } from './layer-selector.component';
 import { ConditionalWrapperComponent } from './conditional-wrapper/conditional-wrapper.component';
 import { ExpandableContainerComponent } from './expandable-container/expandable-container.component';
+import { LayerSelectorItemComponent } from './layer-selector-item/layer-selector-item.component';
 
 
 @NgModule({
-  declarations: [LayerSelectorComponent, ConditionalWrapperComponent, ExpandableContainerComponent],
+  declarations: [LayerSelectorComponent, ConditionalWrapperComponent, ExpandableContainerComponent, LayerSelectorItemComponent],
   imports: [BrowserModule],
   exports: [LayerSelectorComponent]
 })
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import Map from 'arcgis-js-api/Map';
+import MapView from 'arcgis-js-api/views/MapView';
 import { Observable } from 'rxjs';
 
 @Component({
@@ -6,9 +8,27 @@ import { Observable } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.styl']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   findResult: string;
   apiKey = 'AGRC-16E793E2220676';
+  quadWord = '??';
+  mapView: any;
+
+  @ViewChild('mapViewNode', { static: true }) private mapViewEl: ElementRef;
+  view: any;
+
+  ngOnInit() {
+    this.initializeMap();
+  }
+
+  async initializeMap() {
+    this.mapView = new MapView({
+      container: this.mapViewEl.nativeElement,
+      center: [-112, 40],
+      zoom: 10,
+      map: new Map()
+    });
+  }
 
   onFind($event: Observable<any>): void {
     this.findResult = '';
